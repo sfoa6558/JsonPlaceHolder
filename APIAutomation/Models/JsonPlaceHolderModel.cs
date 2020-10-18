4a413b17b6ffb300021e90fb8179941443ed6832
Fola Akinyemi <FAkinyemi@paylocity.com>
2020-10-18 10:32:42 -0500

    took out unecessary directives, fixed spacing formating issues, took out unecessary constructors

--- APIAutomation/Models/JsonPlaceHolderModel.cs
+++ APIAutomation/Models/JsonPlaceHolderModel.cs
@@ -1,27 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using RestSharp;
+﻿
 namespace APIAutomation
 {
     public class JsonPlaceHolderModel
     {
-
-        public int id { get; }
-        public JsonPlaceHolderModel()
-        {
-
-
-
-        }
-
+        public int id { get; }        
     }
-
-
-
 
 }
--- APIAutomation/Models/JsonPlaceHolderPostModel.cs
+++ APIAutomation/Models/JsonPlaceHolderPostModel.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using RestSharp;
+
 namespace APIAutomation
 {
     public class JsonPlaceHolderPostModel : JsonPlaceHolderModel
@@ -17,14 +14,11 @@ namespace APIAutomation
         public int UserId { get; set; }
 
        
-
         public JsonPlaceHolderPostModel(int userId, string title, string body)
         {
             this.UserId = userId;
             this.Title = title;
             this.Body = body;
-
-
 
         }
 
